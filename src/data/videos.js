78237faa26388904d901750c72b00dc776b29d01
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-09-20 15:45:32 +0530

    Videos Page Added :alembic:

---
+++ src/data/videos.js
@@ -0,0 +1,30 @@
+const videos = [
+    {
+        title: 'Some Title',
+        desc: 'Some description',
+        youtube: 'https://www.youtube.com/watch?v=PtR5qpoaTGQ',
+    },
+    {
+        title: 'Some Title',
+        desc: 'Some description',
+        youtube: 'https://www.youtube.com/watch?v=PtR5qpoaTGQ',
+    },{
+        title: 'Some Title',
+        desc: 'Some description',
+        youtube: 'https://www.youtube.com/watch?v=PtR5qpoaTGQ',
+    },{
+        title: 'Some Title',
+        desc: 'Some description',
+        youtube: 'https://www.youtube.com/watch?v=PtR5qpoaTGQ',
+    },{
+        title: 'Some Title',
+        desc: 'Some description',
+        youtube: 'https://www.youtube.com/watch?v=PtR5qpoaTGQ',
+    },{
+        title: 'Some Title',
+        desc: 'Some description',
+        youtube: 'https://www.youtube.com/watch?v=PtR5qpoaTGQ',
+    },
+]
+
+export default videos;
