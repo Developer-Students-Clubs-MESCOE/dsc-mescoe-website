881fe1993c90b6b09c0bbdd6b8817c1cbbd45c3b
VarunIrani <varunirani0505@gmail.com>
2020-09-20 15:00:25 +0530

    Resolved Event Page merge conflicts and missing tags

--- src/data/eventSections.js
+++ src/data/eventSections.js
@@ -15,45 +15,45 @@ const eventSections = [
       {
         image: "https://flutter.dev/images/flutter-logo-sharing.png",
         content: ["Flutter Interact Extended"],
-        dateNtime: 1603107587,
+        dateTime: 1603107587,
         location: 'Pune',
         onlineLocation: false
       },
       {
         image: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
         content: ["An Intro to React JS"],
-        dateNtime: 1603193987,
+        dateTime: 1603193987,
         location: 'Google Meet',
-        onlineLocation: true, 
-    },
+        onlineLocation: true,
+      },
       {
         image: "https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1521098155260-OD3QNLD1RK7DGPWMCUA6/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/js.jpg?format=2500w",
         content: ["Code JS not 💩"],
-        dateNtime: 1603193987,
+        dateTime: 1603193987,
         location: 'Pune',
-        onlineLocation: false      
-    },
-    {
-      image: "https://flutter.dev/images/flutter-logo-sharing.png",
-      content: ["Flutter Interact Extended"],
-      dateNtime: 1603107587,
-      location: 'Pune',
-      onlineLocation: false
-    },
-    {
-      image: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
-      content: ["An Intro to React JS"],
-      dateNtime: 1603193987,
-      location: 'Google Meet',
-      onlineLocation: true, 
-    },
-    {
-      image: "https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1521098155260-OD3QNLD1RK7DGPWMCUA6/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/js.jpg?format=2500w",
-      content: ["Code JS not 💩"],
-      dateNtime: 1603193987,
-      location: 'Pune',
-      onlineLocation: false      
-    }
+        onlineLocation: false
+      },
+      {
+        image: "https://flutter.dev/images/flutter-logo-sharing.png",
+        content: ["Flutter Interact Extended"],
+        dateTime: 1603107587,
+        location: 'Pune',
+        onlineLocation: false
+      },
+      {
+        image: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+        content: ["An Intro to React JS"],
+        dateTime: 1603193987,
+        location: 'Google Meet',
+        onlineLocation: true,
+      },
+      {
+        image: "https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1521098155260-OD3QNLD1RK7DGPWMCUA6/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/js.jpg?format=2500w",
+        content: ["Code JS not 💩"],
+        dateTime: 1603193987,
+        location: 'Pune',
+        onlineLocation: false
+      }
     ],
     videos: null,
   },
@@ -69,30 +69,30 @@ const eventSections = [
       {
         image: "https://mk0analyticsindf35n9.kinstacdn.com/wp-content/uploads/2020/01/top-10-DS-projects.png",
         content: ["The longest ever project name possible but 2 lines only."],
-        dateNtime: 1600515587,
+        dateTime: 1600515587,
         location: 'Pune',
-        onlineLocation: false   
+        onlineLocation: false
       },
       {
         image: "https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo",
         content: ["The longest ever project name possible but 2 lines only."],
-        dateNtime: 1600515587,
+        dateTime: 1600515587,
         location: 'Microsoft Teams',
-        onlineLocation: true    
+        onlineLocation: true
       },
       {
         image: "https://files.realpython.com/media/13-Python-Projects-for-Intermediate-Developers_Watermarked.bb98d44bdb10.jpg",
         content: ["The longest ever project name possible but 2 lines only."],
-        dateNtime: 1600515587,
+        dateTime: 1600515587,
         location: 'Pune',
-        onlineLocation: false   
+        onlineLocation: false
       },
       {
         image: "https://files.realpython.com/media/13-Python-Projects-for-Intermediate-Developers_Watermarked.bb98d44bdb10.jpg",
         content: ["The longest ever project name possible but 2 lines only."],
-        dateNtime: 1600515587,
+        dateTime: 1600515587,
         location: 'Google Meet',
-        onlineLocation: true, 
+        onlineLocation: true,
       }
     ],
     videos: null,
