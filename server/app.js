be7c5a459d0889422b30f2f60668092785d662a1
Prem Kulkarni <kulkarniprem04@gmail.com>
2020-09-22 23:47:25 +0530

    finished with the videos route

--- server/app.js
+++ server/app.js
@@ -17,9 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/events', eventRouter);
-app.use('/projects', projectRouter);
-app.use('/videos', videoRouter);
+app.use('/api', indexRouter);
+app.use('/api/events', eventRouter);
+app.use('/api/projects', projectRouter);
+app.use('/api/videos', videoRouter);
 
 module.exports = app;
--- server/db/models/Video.js
+++ server/db/models/Video.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const VideoSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     name: {
         type: String,
         required: true
+    },
+    url: {
+        type: String,
+        required: true
     }
-    
 });
 
-const Video = mongoose.model('video', VideoSchema);
+const Video = mongoose.model("video", VideoSchema);
 module.exports = Video;
