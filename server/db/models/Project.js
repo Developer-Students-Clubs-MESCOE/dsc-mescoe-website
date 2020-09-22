a6fe905e1be777c36d4075fc1b673b1ac39cfcb4
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-09-22 23:08:51 +0530

    Projects API Completed & Added to Frontend :rocket:

--- server/db/models/Project.js
+++ server/db/models/Project.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema({
-    name: {
+    title: {
         type: String,
         required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    youtube: {
+        type: String
+    },
+    github: {
+        type: String
+    },
+    image: {
+        type: String,
+        default: 'https://via.placeholder.com/800x800?text=DSC+MESCOE'
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-    
 });
 
 const Project = mongoose.model('project', ProjectSchema);
