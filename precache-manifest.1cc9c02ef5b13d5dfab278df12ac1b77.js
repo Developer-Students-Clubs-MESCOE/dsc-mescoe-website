83dea0936a9313bcc03fcbc8872f511a70de347f
VarunIrani <varunirani0505@gmail.com>
2020-09-24 11:51:07 +0530

    Updates

---
+++ precache-manifest.1cc9c02ef5b13d5dfab278df12ac1b77.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0de322d81ac675427f34a73b2399c6",
+    "url": "/dsc-website/index.html"
+  },
+  {
+    "revision": "e716cef72b249aa615b6",
+    "url": "/dsc-website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "50a9d572af349b42ddaa",
+    "url": "/dsc-website/static/css/main.4d7eb8f9.chunk.css"
+  },
+  {
+    "revision": "e716cef72b249aa615b6",
+    "url": "/dsc-website/static/js/2.f0f10ea7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/dsc-website/static/js/2.f0f10ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a9d572af349b42ddaa",
+    "url": "/dsc-website/static/js/main.19931404.chunk.js"
+  },
+  {
+    "revision": "837ee29dbe717c5a2a38",
+    "url": "/dsc-website/static/js/runtime-main.22e97026.js"
+  },
+  {
+    "revision": "38e52b7fa66d9c709f85fde33dd2b0a3",
+    "url": "/dsc-website/static/media/Dhruvil.38e52b7f.jpg"
+  },
+  {
+    "revision": "e58ffdd16d42c7a6dc29ea7fa38d6a29",
+    "url": "/dsc-website/static/media/Gaurav.e58ffdd1.jpg"
+  },
+  {
+    "revision": "494634e149f5b10fdd7de44df351a40b",
+    "url": "/dsc-website/static/media/Kaif.494634e1.jpg"
+  },
+  {
+    "revision": "a7cce155a7e3fa9353b7c4407b107d77",
+    "url": "/dsc-website/static/media/Karan.a7cce155.jpg"
+  },
+  {
+    "revision": "6bdbbcf697ed1f3b49f65d1af9f53189",
+    "url": "/dsc-website/static/media/Laukik.6bdbbcf6.jpg"
+  },
+  {
+    "revision": "c35b2749f0e34fd9a2e65e7c54a7848e",
+    "url": "/dsc-website/static/media/Pratik.c35b2749.jpg"
+  },
+  {
+    "revision": "7c541b9cdc70f261afd7e0949df6754b",
+    "url": "/dsc-website/static/media/Prem.7c541b9c.jpg"
+  },
+  {
+    "revision": "059ff9692141622193cc31b8844baff0",
+    "url": "/dsc-website/static/media/Sakshi.059ff969.jpg"
+  },
+  {
+    "revision": "c5f78bdfd328beb0a4d9cdc1937b85e0",
+    "url": "/dsc-website/static/media/Tanya.c5f78bdf.jpg"
+  },
+  {
+    "revision": "6a4efdb70472e24922a9aff06aacfee8",
+    "url": "/dsc-website/static/media/Varun.6a4efdb7.jpg"
+  },
+  {
+    "revision": "3550ec3d10a624056496a962bfb2afed",
+    "url": "/dsc-website/static/media/down-arrow.3550ec3d.svg"
+  },
+  {
+    "revision": "ddc5add1c07626614725042d456a0144",
+    "url": "/dsc-website/static/media/dsc_home_image.ddc5add1.webp"
+  },
+  {
+    "revision": "bd954ceeeb42b91899ebe1c3b0d79850",
+    "url": "/dsc-website/static/media/gdg_logo.bd954cee.png"
+  },
+  {
+    "revision": "d02663a4803868f5f4c69e99d1ba79f2",
+    "url": "/dsc-website/static/media/gdg_wordmark.d02663a4.png"
+  },
+  {
+    "revision": "37501b6acf969e13d939a97564c89541",
+    "url": "/dsc-website/static/media/team.37501b6a.webp"
+  },
+  {
+    "revision": "26ed4dc2e7b309b26c771a199ed58d33",
+    "url": "/dsc-website/static/media/trialNoGridFinal.26ed4dc2.svg"
+  }
+]);
