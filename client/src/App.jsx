72b56868ff2fb730f5c9e1f7a4c12369e90bf28d
VarunIrani <varunirani0505@gmail.com>
2020-12-16 14:04:45 +0530

    Added Redirect Component. Generalized for any path and any url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,61 +1,63 @@
 import React from 'react';
-import {Hidden, Toolbar} from '@material-ui/core';
-import {Switch, Route, HashRouter as Router} from 'react-router-dom';
+import { Hidden, Toolbar } from '@material-ui/core';
+import { Switch, Route, HashRouter as Router } from 'react-router-dom';
 import DSCNavBar from './components/navigation/DSCNavBar';
-import ROUTES, {RouteType} from './routes';
+import ROUTES, { RouteType } from './routes';
 import Footer from './components/navigation/Footer';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleScroll = this.handleScroll.bind(this)
-  }
+	constructor(props) {
+		super(props);
+		this.handleScroll = this.handleScroll.bind(this);
+	}
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-    if (localStorage.getItem('isDarkMode') === null) {
-      localStorage.setItem('isDarkMode', JSON.stringify(false));
-    }
-  }
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+		if (localStorage.getItem('isDarkMode') === null) {
+			localStorage.setItem('isDarkMode', JSON.stringify(false));
+		}
+	}
 
-  handleScroll() {
-    const footer = document.querySelector('#contact');
-    if (footer) {
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        if (footer.classList.contains('fade-out-animation'))
-          footer.classList.replace('fade-out-animation', 'fade-in-animation')
-        else
-          footer.classList.add('fade-in-animation')
-      } else {
-        if (footer.classList.contains('fade-in-animation'))
-          footer.classList.replace('fade-in-animation', 'fade-out-animation')
-        else
-          footer.classList.add('fade-out-animation')
-      }
-    }
-  }
+	handleScroll() {
+		const footer = document.querySelector('#contact');
+		if (footer) {
+			if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+				if (footer.classList.contains('fade-out-animation'))
+					footer.classList.replace('fade-out-animation', 'fade-in-animation');
+				else footer.classList.add('fade-in-animation');
+			} else {
+				if (footer.classList.contains('fade-in-animation'))
+					footer.classList.replace('fade-in-animation', 'fade-out-animation');
+				else footer.classList.add('fade-out-animation');
+			}
+		}
+	}
 
-  getRoutes(routes) {
-    return routes.filter(r => r.type === RouteType.PAGE || r.type === RouteType.FOOTER).map(
-      (prop, key) =>
-        prop.path === '/' ? (
-          <Route exact path={prop.path} key={key} component={prop.component}/>
-        ) : (
-          <Route path={prop.path} key={key} component={prop.component}/>
-        )
-    );
-  }
+	getRoutes(routes) {
+		return routes.filter((r) => r.type === RouteType.PAGE || r.type === RouteType.FOOTER).map(
+			(prop, key) =>
+				prop.path === '/' ? (
+					<Route exact path={prop.path} key={key}>
+						{prop.component}
+					</Route>
+				) : (
+					<Route path={prop.path} key={key}>
+						{prop.component}
+					</Route>
+				)
+		);
+	}
 
-  render() {
-    return (
-      <Router basename='/'>
-        <DSCNavBar/>
-        <Toolbar/>
-        <Switch>{this.getRoutes(ROUTES)}</Switch>
-        <Hidden xsDown>
-          <Footer/>
-        </Hidden>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router basename="/">
+				<DSCNavBar />
+				<Toolbar />
+				<Switch>{this.getRoutes(ROUTES)}</Switch>
+				<Hidden xsDown>
+					<Footer />
+				</Hidden>
+			</Router>
+		);
+	}
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -4,17 +4,24 @@ import Events from './views/Events';
 import Videos from './views/Videos';
 import Projects from './views/Projects';
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {Movie, People, Today, HomeRounded as HomeIcon, DeveloperBoard} from "@material-ui/icons";
-import {faMedium} from '@fortawesome/free-brands-svg-icons'
-import gdgBlack from './assets/img/gdg_black.png'
-import {Image} from "react-bootstrap";
-import GCPRank from "./views/GCPRank";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Movie, People, Today, HomeRounded as HomeIcon, DeveloperBoard } from '@material-ui/icons';
+import { faMedium } from '@fortawesome/free-brands-svg-icons';
+import gdgBlack from './assets/img/gdg_black.png';
+import { Image } from 'react-bootstrap';
+import GCPRank from './views/GCPRank';
 
 export const RouteType = {
 	LINK: 0,
 	PAGE: 1,
 	FOOTER: 2
+};
+
+class Redirect extends React.Component {
+	componentDidMount() {
+		if (window.location.href.includes(this.props.path)) window.location.replace(this.props.url);
+	}
+	render = () => <React.Fragment />;
 }
 
 const ROUTES = [
@@ -22,58 +29,70 @@ const ROUTES = [
 		icon: null,
 		name: 'GCP Rank',
 		path: '/gcp-rank',
-		component: GCPRank,
+		component: <GCPRank />,
 		type: RouteType.PAGE
 	},
 	{
-		icon: <HomeIcon style={{color: 'black'}} />,
+		icon: <HomeIcon style={{ color: 'black' }} />,
 		name: 'Home',
 		path: '/',
-		component: Home,
+		component: <Home />,
 		type: RouteType.PAGE
 	},
 	{
-		icon: <People style={{color: 'black'}} />,
+		icon: <People style={{ color: 'black' }} />,
 		name: 'Team',
 		path: '/team',
-		component: Team,
+		component: <Team />,
 		type: RouteType.PAGE
 	},
 	{
-		icon: <Today style={{color: 'black'}} />,
+		icon: <Today style={{ color: 'black' }} />,
 		name: 'Events',
 		path: '/events',
-		component: Events,
+		component: <Events />,
 		type: RouteType.PAGE
 	},
 	{
-		icon: <Movie style={{color: 'black'}} />,
+		icon: <Movie style={{ color: 'black' }} />,
 		name: 'Videos',
 		path: '/videos',
-		component: Videos,
+		component: <Videos />,
 		type: RouteType.PAGE
 	},
 	{
-		icon: <DeveloperBoard style={{color: 'black'}} />,
+		icon: <DeveloperBoard style={{ color: 'black' }} />,
 		name: 'Projects',
 		path: '/projects',
-		component: Projects,
+		component: <Projects />,
 		type: RouteType.PAGE
 	},
 	{
-		icon: <FontAwesomeIcon style={{color: 'black', fontSize: 26}} icon={faMedium}/>,
+		icon: <FontAwesomeIcon style={{ color: 'black', fontSize: 26 }} icon={faMedium} />,
 		name: 'Blogs',
 		path: 'https://medium.com/@dscmescoe',
 		component: null,
 		type: RouteType.LINK
 	},
 	{
-		icon: <Image src={gdgBlack} style={{width: '1.5em'}} />,
+		icon: <Image src={gdgBlack} style={{ width: '1.5em' }} />,
 		name: 'Community Page',
 		path: 'https://dsc.community.dev/modern-education-societys-college-of-engineering/',
 		component: null,
 		type: RouteType.LINK
 	},
+	{
+		icon: null,
+		name: 'Android Study Jam',
+		path: '/android-session-1',
+		component: (
+			<Redirect
+				path="android-session-1"
+				url="https://teams.microsoft.com/l/meetup-join/19%3ameeting_NzVhNTliOGQtMTkxZi00ZjgwLWIwN2UtZmI1YTk2M2NhZjA4%40thread.v2/0?context=%7b%22Tid%22%3a%22a3d598b6-6092-4351-b1a3-b213e8adc03b%22%2c%22Oid%22%3a%22661f07ac-f916-4ef3-a486-c25860d7f28e%22%7d"
+			/>
+		),
+		type: RouteType.PAGE
+	}
 ];
 
 export default ROUTES;
