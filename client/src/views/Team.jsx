710209c666ab6a0bf2dad4ca4f7edbebd8ca2b34
shreyash <shreyashyewale84964@gmail.com>
2021-08-16 00:40:54 +0530

    Third Commit

--- client/src/views/Team.jsx
+++ client/src/views/Team.jsx
@@ -7,33 +7,48 @@ import TeamComponent from '../components/team/team-component';
 import TeamData from './TeamData.js';
 import Karan from '../assets/img/Karan.jpg';
 import Dhruvil from '../assets/img/Dhruvil.jpg';
-const TeamMember = (member) => {
-  return (
-    <div className="person">
-  <div className="person-badge">
-        <img className="normal" src={member.imgn.Karan}/>
-        <img className="peculiar" src={member.imgp.Dhruvil}/>
-  </div>
-  <div className="info">
-    <h4>Vaibhav Bhapkar</h4>
-    <h5>Lead</h5>
-  </div>
-  <div className="links">
-    <p>😄</p>
-    <p>😄</p>
-  </div>
-</div>
-  );
-};
-
-          
 export default class Team extends React.Component {
-  
   render() {
     return (
       <div>
-        <div className='col-md-6'></div>
-        {TeamMember(TeamData[0][0])}
+        <div className='row align-items-center'>
+          <div className='col-6 offset-1 mt-4'>
+            <img
+              className='mt-4 ml-4'
+              src={process.env.PUBLIC_URL + '/Images/firstparallel.jpg'}
+              alt='heading_illustration'
+              style={{ height: '470px', width: '560px' }}
+            />
+          </div>
+          <div className='col-5'>
+            <p style={{ fontSize: '28px' }}>
+              We United to be Strong!!
+              afdafdsfsafasdfasdfasdfasdf
+              asdfasdfsadfsdafdsfdasfasdfs
+              sadfsdafdsafadsfasdfdsfdsfdsf
+            </p>
+          </div>
+        </div>
+        <div className='container'>
+          {TeamData.map((item, index) =>
+            item.map((member, subindex) => (
+              <div className={member.classname1}>
+                <div className={member.classname}>
+                  <img className='normal' src={member.imgn.Karan} />
+                  <img className='peculiar' src={member.imgp.Dhruvil} />
+                </div>
+                <div className='info'>
+                  <h4>{member.name}</h4>
+                  <h5>{member.role}</h5>
+                </div>
+                <div className='links'>
+                  <p>😄</p>
+                  <p>😄</p>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     );
   }
--- client/src/views/TeamData.js
+++ client/src/views/TeamData.js
@@ -1,112 +1,138 @@
 import Karan from '../assets/img/Karan.jpg';
 import Dhruvil from '../assets/img/Dhruvil.jpg';
 const TeamData = [
-    [
+  [
     {
-      name: 'Vaibhav',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
-      linkedInLink: '',
-      githubLink: ''
-    }
-  ],
-   [
-    {
-      name: '',
-      imgn: { Karan },
-      imgp: { Dhruvil },
-      linkedInLink: '',
-      githubLink: ''
-    },
-    {
-      name: '',
-      imgn: { Karan },
-      imgp: { Dhruvil },
-      linkedInLink: '',
-      githubLink: ''
-    },
-    {
-      name: '',
-      imgn: { Karan },
-      imgp: { Dhruvil },
-      linkedInLink: '',
-      githubLink: ''
-    },
-    {
-      name: '',
-      imgn: { Karan },
-      imgp: { Dhruvil },
-      linkedInLink: '',
-      githubLink: ''
-    },
-    {
-      name: '',
-      imgn: { Karan },
-      imgp: { Dhruvil },
-      linkedInLink: '',
-      githubLink: ''
+      linkedInLink: 'adfsfafd',
+      githubLink: 'asdfasfdsf',
+      classname:
+        'person-badge ${classes.large} shadow-lg col d-flex justify-content-center',
+      classname1: 'row person mt-4',
+      role: 'Lead'
     }
   ],
   [
     {
-      name: '',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
       linkedInLink: '',
-      githubLink: ''
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg',
+      classname1: 'row person mt-4',
+      role: 'Technical Core'
     },
     {
-      name: '',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
       linkedInLink: '',
-      githubLink: ''
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg',
+      classname1: 'person mt-4',
+      role: 'Technical Core'
+    },
+    {
+      name: 'Vaibhav Bhapkar',
+      imgn: { Karan },
+      imgp: { Dhruvil },
+      linkedInLink: '',
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg',
+      classname1: 'person mt-4'
+    },
+    {
+      name: 'Vaibhav Bhapkar',
+      imgn: { Karan },
+      imgp: { Dhruvil },
+      linkedInLink: '',
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg',
+      classname1: 'person mt-4'
+    },
+    {
+      name: 'Vaibhav Bhapkar',
+      imgn: { Karan },
+      imgp: { Dhruvil },
+      linkedInLink: '',
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg',
+      classname1: 'person mt-4'
     }
   ],
-   [
+  [
     {
-      name: '',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
       linkedInLink: '',
-      githubLink: ''
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg',
+      classname1: ' row person mt-4'
     },
     {
-      name: '',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
       linkedInLink: '',
-      githubLink: ''
-    },
-    {
-      name: '',
-      imgn: { Karan },
-      imgp: { Dhruvil },
-      linkedInLink: '',
-      githubLink: ''
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg',
+      classname1: 'row person mt-4'
     }
   ],
-    [
+  [
     {
-      name: '',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
       linkedInLink: '',
-      githubLink: ''
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg'
     },
     {
-      name: '',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
       linkedInLink: '',
-      githubLink: ''
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg'
     },
     {
-      name: '',
+      name: 'Vaibhav Bhapkar',
       imgn: { Karan },
       imgp: { Dhruvil },
       linkedInLink: '',
-      githubLink: ''
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg'
+    }
+  ],
+  [
+    {
+      name: 'Vaibhav Bhapkar',
+      imgn: { Karan },
+      imgp: { Dhruvil },
+      linkedInLink: '',
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg'
+    },
+    {
+      name: 'Vaibhav Bhapkar',
+      imgn: { Karan },
+      imgp: { Dhruvil },
+      linkedInLink: '',
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg'
+    },
+    {
+      name: 'Vaibhav Bhapkar',
+      imgn: { Karan },
+      imgp: { Dhruvil },
+      linkedInLink: '',
+      githubLink: '',
+      classname: 'person-badge ${classes.large} shadow-lg'
     }
   ]
 ];
