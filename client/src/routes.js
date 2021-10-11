c4dbc66468f2bca2b8d0f80fc9b6692a6faf82ea
Manav <manavmandalpune@gmail.com>
2021-10-11 10:42:16 +0530

    added hacktoberfest meeting link

--- client/src/routes.js
+++ client/src/routes.js
@@ -88,12 +88,12 @@ const ROUTES = [
   },
   {
     icon: null,
-    name: 'GCP Tutorial',
-    path: '/gcp-tutorial',
+    name: 'Hacktoberfest',
+    path: '/hacktoberfest',
     component: (
       <Redirect
-        path='gcp-tutorial'
-        url='https://teams.microsoft.com/l/meetup-join/19%3ameeting_NjU1NmExYzgtOGMwMC00ZjA2LThiYzMtYjE3OTU1M2RkNzUw%40thread.v2/0?context=%7b%22Tid%22%3a%22a3d598b6-6092-4351-b1a3-b213e8adc03b%22%2c%22Oid%22%3a%22105d357b-9536-4050-bdb5-0d5c3c247423%22%7d'
+        path='hacktoberfest'
+        url='https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_OGY4ZDBhMWMtYmJiMi00ODVmLWJhNWMtNDk3YjRlMmRhZjA1%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%25223a3cd3f3-9917-40dc-91e0-85146eaf5d55%2522%252c%2522Oid%2522%253a%25227c041bd0-f19d-4bf9-897f-497793409f05%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=8711275e-1ea5-478c-8f5e-44f8c06e3f6b&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true'
       />
     ),
     type: RouteType.PAGE,
