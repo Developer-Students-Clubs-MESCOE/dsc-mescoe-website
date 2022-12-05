13edb93b8dbb31e984604300efb5622290161b21
miketailorglobal <miketailorglobal@gmail.com>
2022-12-06 02:28:34 +0530

    first commit gdsc backend

--- db/default.js
+++ db/default.js
@@ -1,5 +1,12 @@
 const config = {
-  mongoURI: `mongodb+srv://admin:${process.env.DB_PASS}@dsc.xen2d.gcp.mongodb.net/dsc-website?retryWrites=true&w=majority`
+  mongoURI: `mongodb+srv://gdsc:gdsc@gdsc.mjz5pb0.mongodb.net/?retryWrites=true&w=majority`
 };
 
 module.exports = config
+
+
+// const config = {
+//   mongoURI: `mongodb+srv://admin:${process.env.DB_PASS}@dsc.xen2d.gcp.mongodb.net/dsc-website?retryWrites=true&w=majority`
+// };
+
+// module.exports = config
